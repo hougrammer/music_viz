4a398d2798ae8876e3bf62e617f20bfc06cb13da
hougrammer <david.m.hou@gmail.com>
2018-10-26 23:35:52 -0700

    implemented reading from url list

--- parse.js
+++ parse.js
@@ -1,14 +1,27 @@
 /* global require */
 const ugs = require('../ultimate-guitar-scraper/lib/index.js');
-const utils = require('./utils');
+const utils = require('./utils.js');
 const fs = require('fs');
 
-const tabUrl = 'https://tabs.ultimate-guitar.com/tab/imagine_dragons/radioactive_chords_1171909';
+let urls = fs.readFileSync('data/urlList.txt', 'utf8').split('\n');
+let parsedSongs = JSON.parse(fs.readFileSync('data/parsedSongs.json', 'utf8'));
+let skipSongs = new Set(parsedSongs.map(x => x.name));
 
-ugs.get(tabUrl, (error, tab) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(utils.extractRawChords(tab.content.text));
-  }
+let songs = utils.parseUrlList(urls, skipSongs, {'decade': '2010'});
+Promise.all(songs).then(results => {
+  parsedSongs.push(...results.filter(x => x !== null));
+  fs.writeFileSync('data/parsedSongs.json', JSON.stringify(parsedSongs), 'utf8');
 });
+
+// ugs.search({
+//   query: 'Wish You Were Here Pink Floyd',
+//   page: 1,
+//   type: 'Chords'
+// }, (error, tabs) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     tabs.sort((a,b) => b.numberRates - a.numberrates);
+//     console.log(tabs);
+//   }
+// });
--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
-/* global module */
+/* global module require */
+const ugs = require('../ultimate-guitar-scraper/lib/index.js');
+const fs = require('fs');
+
+const debug = true;
 
 /**
  * @param  {string} text of tab.content
@@ -7,9 +11,94 @@
 function extractRawChords(text) {
   return text
     .match(/\[ch\].*?\[\/ch\]/g) // grab all the chords
-    .map(x => x.substring(4, x.length-5)); // throw out the [ch] and [/ch]
+    .map(x => x.substring(4, x.length - 5)); // throw out the [ch] and [/ch]
 }
 
+// const promises = []  // collect all promises here
+// items.forEach(item => {
+//     const promise = item.doWork()
+//     promises.push(promise)
+// })
+// Promise.all(promises).then(results => {
+//     // continue processing here
+//     // results[0] is the result of the first promise in the promises array
+// })
+
+function parseUrlList(urls, skipSongs, info) {
+  let songs = [];
+
+  urls.forEach((url) => {
+    let songPromise = new Promise((resolve, reject) => {
+      ugs.get(url, (err, tab) => {
+        if (err) reject(err);
+
+        if (skipSongs.has(tab.name)) {
+          if (debug) console.log('Skipping ' + tab.name + ' because it has already been parsed.');
+          resolve(null);
+        }
+        else {
+          if (debug) console.log('Parsing ' + tab.name + '...');
+
+          let song = {};
+          song.name = tab.name;
+          song.artist = tab.artist;
+          song.capo = tab.capo;
+          song.tonality = tab.tonality;
+          song.tuning = tab.tuning;
+          song.chords = extractRawChords(tab.content.text);
+          song.decade = info.decade;
+
+          skipSongs.add(song.name);
+          resolve(song);
+        }
+        
+      });
+    });
+    songs.push(songPromise);
+  });
+  return songs;
+}
+
+// function parseUrlList(urls, skipSongs, info) {
+//   return new Promise((resolve, reject) => {
+//     let songs = [];
+//     let i = 0;
+//     urls.forEach((url, _, array) => {
+
+//       ugs.get(url, (err, tab) => {
+//         if (err) reject(err);
+
+//         i++;
+//         console.log(i, array.length, songs.length)
+//         if (skipSongs.has(tab.name)) {
+//           if (debug) console.log('Skipping ' + tab.name + ' because it has already been parsed.');
+//           return;
+//         }
+//         if (debug) console.log('Parsing ' + tab.name + '...');
+
+//         let song = {};
+//         song.name = tab.name;
+//         song.artist = tab.artist;
+//         song.capo = tab.capo;
+//         song.tonality = tab.tonality;
+//         song.tuning = tab.tuning;
+//         song.chords = extractRawChords(tab.content.text);
+//         song.decade = info.decade;
+
+//         skipSongs.add(song.name);
+//         songs.push(song);
+//         if (i === array.length-1) {
+//           resolve(songs);
+//         }
+//       });
+
+//     });
+
+//   });
+
+// }
+
 module.exports = {
-  extractRawChords
+  extractRawChords,
+  parseUrlList
 };
