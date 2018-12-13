9952ac22e0434a205126096cc214b6a800e1a943
esko22 <kbloom@lunadna.com>
2018-12-12 22:47:42 -0800

    checkpoint wed 10

--- js/artist_specs.js
+++ js/artist_specs.js
@@ -1,9 +1,11 @@
+// TODO: Issue with default filter - https://github.com/vega/vega-lite/issues/3409
+
 var artistTrackAggregateSpec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
     "data": { "name": "music-billboard-data" },
-    "hconcat": [
+    "vconcat": [
       {
-        "width" : 350, "height" : 200,
+        "width" : 700, "height" : 300,
             "mark": "bar",
             "encoding": {
                 "y": {
@@ -15,29 +17,98 @@ var artistTrackAggregateSpec = {
                 "field": "weeks", 
                 "type": "quantitative",
                 "axis": {"title": "weeks on chart"}
+              },
+              "color": {
+                "condition": {
+                  "selection": "click",
+                  "value": "red"
+                },
+                "value": "grey"
               }
             },
-            "selection": {"click": {"encodings": ["y"], "type": "multi"}},
+            "selection": {"click": {"encodings": ["y"], "type": "multi", "empty": "all"}},
             "transform": [
+              {"filter": { "field": "artist", "oneOf" : artists }},
               {"filter": { "selection": "artists"}},
               {"sample": 30},
             ]
         },
-        {
-          "width" : 350, "height" : 200,
+        {          
+          "hconcat": [
+          {
+            "width" : 200, "height" : 150,
+            "transform": [
+              {"filter": { "field": "artist", "oneOf" : artists }},
+              {"filter": { "selection": "artists"}},
+              {"filter": {"selection": "click"}}
+            ],
+            "mark": "circle",
+            "encoding": {
+              "y": {
+                "field": "peak_pos",
+                "sort" : "descending",
+                "type": "quantitative",
+                "scale": {"domain": [0,100]}
+              },
+              "x": {
+                "bin": {"step" : .5},
+                "field": "energy",
+                "type": "quantitative",
+              },
+              "size": {
+                "field": "weeks",
+                "type": "quantitative",
+                "aggregate": "sum"
+              }
+            },
+          },
+          {
+            "width" : 200, "height" : 150,
+            "transform": [
+              {"filter": { "field": "artist", "oneOf" : artists }},
+              {"filter": { "selection": "artists"}},
+              {"filter": {"selection": "click"}}
+            ],
+            "mark": "circle",
+            "encoding": {
+              "y": {
+                "field": "peak_pos",
+                "sort" : "descending",
+                "type": "quantitative",
+                "scale": {"domain": [0,100]}
+              },
+              "x": {
+                "bin": {"step" : .5},
+                "field": "tempo",
+                "type": "quantitative",
+              },
+              "size": {
+                "field": "weeks",
+                "type": "quantitative",
+                "aggregate": "sum"
+              }
+            },
+          },  
+          {
+          "width" : 200, "height" : 150,
           "transform": [
+            {"filter": { "field": "artist", "oneOf" : artists }},
+            {"filter": { "selection": "artists"}},
             {"filter": {"selection": "click"}}
           ],
           "mark": "circle",
           "encoding": {
             "y": {
               "field": "peak_pos",
-              "type": "ordinal",
+              "sort" : "descending",
+              "type": "quantitative",
+              "axis": {"title": "Peak Position"},
+              "scale": {"domain": [0,100]}
             },
             "x": {
-              "bin": {"step" : 1.75},
+              "bin": {"step" : 5},
               "field": "loudness",
-              "type": "quantitative"
+              "type": "quantitative",
             },
             "size": {
               "field": "weeks",
@@ -52,7 +123,194 @@ var artistTrackAggregateSpec = {
               "bind": {"input": "select", "name": "Artists", "options": artists}
             }
           }
+
+        }
+      ]
+    },
+    {          
+      "hconcat": [
+      {
+        "width" : 200, "height" : 150,
+        "transform": [
+          {"filter": { "field": "artist", "oneOf" : artists }},
+          {"filter": { "selection": "artists"}},
+          {"filter": {"selection": "click"}}
+        ],
+        "mark": "circle",
+        "encoding": {
+          "y": {
+            "field": "peak_pos",
+            "sort" : "descending",
+            "type": "quantitative",
+            "scale": {"domain": [0,100]}
+          },
+          "x": {
+            "bin": {"step" : .5},
+            "field": "danceability",
+            "type": "quantitative",
+          },
+          "size": {
+            "field": "weeks",
+            "type": "quantitative",
+            "aggregate": "sum"
+          }
         },
-    ]
+      },
+      {
+        "width" : 200, "height" : 150,
+        "transform": [
+          {"filter": { "field": "artist", "oneOf" : artists }},
+          {"filter": { "selection": "artists"}},
+          {"filter": {"selection": "click"}}
+        ],
+        "mark": "circle",
+        "encoding": {
+          "y": {
+            "field": "peak_pos",
+            "sort" : "descending",
+            "type": "quantitative",
+            "scale": {"domain": [0,100]}
+          },
+          "x": {
+            "bin": {"step" : .5},
+            "field": "speechiness",
+            "type": "quantitative",
+            "axis": {"maxExtent": 5},
+          },
+          "size": {
+            "field": "weeks",
+            "type": "quantitative",
+            "aggregate": "sum"
+          }
+        },
+      },  
+      {
+      "width" : 200, "height" : 150,
+      "transform": [
+        {"filter": { "field": "artist", "oneOf" : artists }},
+        {"filter": { "selection": "artists"}},
+        {"filter": {"selection": "click"}}
+      ],
+      "mark": "circle",
+      "encoding": {
+        "y": {
+          "field": "peak_pos",
+          "sort" : "descending",
+          "type": "quantitative",
+          "axis": {"title": "Peak Position"},
+          "scale": {"domain": [0,100]}
+        },
+        "x": {
+          "bin": {"step" : 5},
+          "field": "instrumentalness",
+          "type": "quantitative",
+          "axis": {"maxExtent": 5},
+        },
+        "size": {
+          "field": "weeks",
+          "type": "quantitative",
+          "aggregate": "sum"
+        }
+      },
+      "selection": {
+        "artists": {
+          "type": "single",
+          "fields": ["artist"],
+          "bind": {"input": "select", "name": "Artists", "options": artists}
+        }
+      }
+
+    }
+  ]
+  },
+  {          
+    "hconcat": [
+    {
+      "width" : 200, "height" : 150,
+      "transform": [
+        {"filter": { "field": "artist", "oneOf" : artists }},
+        {"filter": { "selection": "artists"}},
+        {"filter": {"selection": "click"}}
+      ],
+      "mark": "circle",
+      "encoding": {
+        "y": {
+          "field": "peak_pos",
+          "sort" : "descending",
+          "type": "quantitative",
+          "scale": {"domain": [0,100]}
+        },
+        "x": {
+          "bin": {"step" : 1},
+          "field": "key",
+          "type": "quantitative",
+        },
+        "size": {
+          "field": "weeks",
+          "type": "quantitative",
+          "aggregate": "sum"
+        }
+      },
+    },
+    {
+      "width" : 200, "height" : 150,
+      "transform": [
+        {"filter": { "field": "artist", "oneOf" : artists }},
+        {"filter": { "selection": "artists"}},
+        {"filter": {"selection": "click"}}
+      ],
+      "mark": "circle",
+      "encoding": {
+        "y": {
+          "field": "peak_pos",
+          "sort" : "descending",
+          "type": "quantitative",
+          "scale": {"domain": [0,100]}
+        },
+        "x": {
+          "bin": {"step" : .5},
+          "field": "valence",
+          "type": "quantitative",
+          "axis": {"maxExtent": 5},
+        },
+        "size": {
+          "field": "weeks",
+          "type": "quantitative",
+          "aggregate": "sum"
+        }
+      },
+    },  
+    {
+    "width" : 200, "height" : 150,
+    "transform": [
+      {"filter": { "field": "artist", "oneOf" : artists }},
+      {"filter": { "selection": "artists"}},
+      {"filter": {"selection": "click"}}
+    ],
+    "mark": "circle",
+    "encoding": {
+      "y": {
+        "field": "peak_pos",
+        "sort" : "descending",
+        "type": "quantitative",
+        "axis": {"title": "Peak Position"},
+        "scale": {"domain": [0,100]}
+      },
+      "x": {
+        "bin": {"step" : 5},
+        "field": "time_signature",
+        "type": "quantitative",
+        "axis": {"maxExtent": 5},
+      },
+      "size": {
+        "field": "weeks",
+        "type": "quantitative",
+        "aggregate": "sum"
+      }
+    }
   }
+]
+}
+]
+}
   
--- js/charteristic_specs.js
+++ js/charteristic_specs.js
@@ -30,28 +30,226 @@ var musicChars1Spec =  {
         "type": "quantitative",
         "aggregate": "sum"
       }
+      }
     }
-  },
-  {
-    "mark": "circle",
+]
+}
+
+
+var genreHeatMapSpec = 
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+  "data": { "name": "music-billboard-data" },
+
+
+  "vconcat" : [{
+
+    "hconcat" : [{
+      "transform": [{"filter": { "selection": "genre"}}],
+      "selection": {
+        "genre": {
+          "type": "single",
+          "fields": ["broad_genre"],
+          "bind": {"input": "select", "name": "Genre", "options": genres}
+        }
+      },
+    
+      "mark": "rect",
+      "width": 250,
+      "height": 200,
+      "encoding": {
+        "x": {
+          "bin": {"step" : .125},
+          "field": "energy",
+          "type": "quantitative"
+        },
+        "y": {
+          "field": "year",
+          "type": "ordinal",
+          "timeUnit": "year",
+          "axis" : {"title": null}
+        },
+        "color": {
+          "aggregate": "count",
+          "type": "quantitative"
+        },
+      }
+      },
+      {
+      "transform": [{"filter": { "selection": "genre"}}],
+      "selection": {
+        "genre": {
+          "type": "single",
+          "fields": ["broad_genre"],
+          "bind": {"input": "select", "name": "Genre", "options": genres}
+        }
+      },
+    
+      "mark": "rect",
+      "width": 250,
+      "height": 200,
+      "encoding": {
+        "x": {
+          "bin": {"step" : 25},
+          "field": "tempo",
+          "type": "quantitative"
+        },
+        "y": {
+          "field": "year",
+          "type": "ordinal",
+          "timeUnit": "year",
+          "axis" : null
+        },
+        "color": {
+          "aggregate": "count",
+          "type": "quantitative"
+        },
+      }
+      },
+      {
+        "transform": [{"filter": { "selection": "genre"}}],
+        "selection": {
+          "genre": {
+            "type": "single",
+            "fields": ["broad_genre"],
+            "bind": {"input": "select", "name": "Genre", "options": genres}
+          }
+        },
+      
+        "mark": "rect",
+        "width": 250,
+        "height": 200,
+        "encoding": {
+          "x": {
+            "bin": {"step" : .125},
+            "field": "danceability",
+            "type": "quantitative"
+          },
+          "y": {
+            "field": "year",
+            "type": "ordinal",
+            "timeUnit": "year",
+            "axis" : null
+          },
+          "color": {
+            "aggregate": "count",
+            "type": "quantitative"
+          },
+        }
+        }
+    
+    ]},
+    {  
+    "hconcat": [{
+    "transform": [{"filter": { "selection": "genre"}}],
+    "selection": {
+      "genre": {
+        "type": "single",
+        "fields": ["broad_genre"],
+        "bind": {"input": "select", "name": "Genre", "options": genres}
+      }
+    },
+  
+    "mark": "rect",
+    "width": 250,
+    "height": 200,
     "encoding": {
+      "x": {
+        "bin": {"step" : .125},
+        "field": "energy",
+        "type": "quantitative"
+      },
       "y": {
         "field": "year",
         "type": "ordinal",
-        "timeUnit": "year"
+        "timeUnit": "year",
+        "axis" : {"title": null}
       },
-      "x": {
-        "bin": {"step" : 0.025},
-        "field": "liveness",
+      "color": {
+        "aggregate": "count",
         "type": "quantitative"
       },
-      "size": {
-        "field": "weeks",
-        "type": "quantitative",
-        "aggregate": "sum"
-      }
     }
-  }]
+    },
+    {
+    "transform": [{"filter": { "selection": "genre"}}],
+    "selection": {
+      "genre": {
+        "type": "single",
+        "fields": ["broad_genre"],
+        "bind": {"input": "select", "name": "Genre", "options": genres}
+      }
+    },
+  
+    "mark": "rect",
+    "width": 250,
+    "height": 200,
+    "encoding": {
+      "x": {
+        "bin": {"step" : 25},
+        "field": "tempo",
+        "type": "quantitative"
+      },
+      "y": {
+        "field": "year",
+        "type": "ordinal",
+        "timeUnit": "year",
+        "axis" : null
+      },
+      "color": {
+        "aggregate": "count",
+        "type": "quantitative"
+      },
+    }
+    },
+    {
+      "transform": [{"filter": { "selection": "genre"}}],
+      "selection": {
+        "genre": {
+          "type": "single",
+          "fields": ["broad_genre"],
+          "bind": {"input": "select", "name": "Genre", "options": genres}
+        }
+      },
+    
+      "mark": "rect",
+      "width": 250,
+      "height": 200,
+      "encoding": {
+        "x": {
+          "bin": {"step" : .125},
+          "field": "danceability",
+          "type": "quantitative"
+        },
+        "y": {
+          "field": "year",
+          "type": "ordinal",
+          "timeUnit": "year",
+          "axis" : null
+        },
+        "color": {
+          "aggregate": "count",
+          "type": "quantitative"
+        },
+      }
+      }
+  
+  ]
+    }],
+  "config": {
+    "range": {
+      "heatmap": {
+        "scheme": "greenblue"
+      }
+    },
+    "view": {
+      "stroke": "transparent"
+    }
+  }
 }
+
+
+
+
 
 
